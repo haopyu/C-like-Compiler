cbd90296ebbc840d3dfcedaa7b756001ccfc2c5c
Molin Deng <zjumorning@gmail.com>
2018-06-08 10:56:25 +0800

    tree DS

---
+++ AST.cpp
@@ -0,0 +1,57 @@
+#include "AST.h"
+//include "yacc.hpp"
+extern FILE* yyin;
+
+
+void PrintTree(AST* tree) {
+
+}
+
+AST* NewNode() {
+    AST* pNew = new AST;
+    if(!pNew) return pNew;
+    memset(pNew, 0, sizeof(*pNew));
+    return pNew;
+}
+
+AST* FatherAddSon(AST* pFather, AST* pSon) {
+    AST* pFS = pFather->lpSub;
+    if (!pFS) { //no son
+        pFather->lpSub = pSon;
+        return pFather;
+    }
+    else {
+        while(pFS->lpNext)//add to neighbor
+            pFS = pFS->lpNext;
+        
+        pFS->lpNext = pSon;
+        return pFather;
+    }
+}
+
+AST* NewFatherAddSon(int iType, int num_of_sons, ...) {
+    AST* pFather = NewNode();
+    if (!pFather)
+    {
+        return pFather;
+    }
+    
+    pFather->type = iType;
+    
+    va_list ap;
+    va_start(ap, num_of_sons);
+    
+    while(num_of_sons--)
+        FatherAddSon(pFather, va_arg(ap, AST*));
+    
+    va_end(ap);
+    
+    return pFather;
+}
+void FreeTree(AST* root) {
+    if (root->lpNext)  FreeTree(root->lpNext);
+    if (root->lpSub)   FreeTree(root->lpSub);
+    //real free
+    delete root;
+    root = NULL;
+}
---
+++ AST.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdarg>
+
+using namespace std;
+
+typedef struct abstract_syntax_tree AST;
+typedef struct symbol Symbol;
+
+enum statement_type {
+    LIST,
+	NUM,
+	STR,
+	SYM,
+	EX_EQ,
+	PLUS_OP,
+	MINUS_OP,
+	MUL_OP,
+	LT_OP,
+	GT_OP,
+	GET_ARRAY_OP,
+	SET_ARRAY_OP,
+	CALL_OP,
+	IF_STATEMENT,
+	BLOCK_STATEMENT,
+	RETURN_STATEMENT,
+	WHILE_STATEMENT,
+	FOR_STATEMENT
+};
+
+struct symbol {
+    char *name;
+	int type, pointer;
+};
+
+struct abstract_syntax_tree {
+    int type;
+    string text;
+    AST *lpNext;// 同一层
+    AST *lpSub;// 子层节点
+};
+
+
+void PrintTree(AST* tree);
+AST* NewNode();
+AST* FatherAddSon(AST* pFather, AST* pSon);
+AST* NewFatherAddSon(int iType, int num_of_sons, ...);
+void FreeTree(AST* root);
+void nm_clear();
+
