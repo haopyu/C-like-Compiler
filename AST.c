f9e72506c60eabf3dbc1558109b5ffecc06d43be
yuhao <withyuh@outlook.com>
2018-06-09 21:32:45 +0800

    refactor

---
+++ AST.c
@@ -0,0 +1,11 @@
+#include "AST.h"
+
+AST* make_node(value val, int _case, ...) {
+
+}
+void print_tree(AST* tree) {
+
+}
+int find_usage(AST* p, String _type[100], int i, String u) {
+    
+}
---
+++ test/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    /* test */
+    int a = 3.5f;
+    //float b = 2.e-1, c = .3f;
+}
